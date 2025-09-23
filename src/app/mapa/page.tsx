38ce311a305ec46745772b3d6f97f111bd426ee4
Rebeca Caroba <rebecacaroba@gmail.com>
2025-09-23 15:27:59 -0300

    Alterando header na pagina mapa

--- src/app/mapa/page.tsx
+++ src/app/mapa/page.tsx
@@ -1,9 +1,11 @@
 import { Header } from "@/components/Header";
 import { MapViewport } from "@/components/MapViewport";
 
+
 export default function Mapa() {
     return (
         <main>
+            <Header />
             <MapViewport />
         </main>
     )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,17 +1,17 @@
 'use client'
-import { RiMenuFold4Line, RiMenuFold3Line  } from "react-icons/ri";
+import { RiMenuFold4Line, RiMenuFold3Line } from "react-icons/ri";
 import Link from 'next/link';
 import logo from '../../../public/aurora-boreal.png'
 import Image from "next/image";
 import './style.scss';
 import { useState } from "react";
+import { usePathname } from "next/navigation";
 
 
 export function Header() {
     const [isMenuBar, setIsMenuBar] = useState<boolean>(false)
-
-    console.log(isMenuBar)
-
+    const router = usePathname();
+    console.log(router)
     return (
         <header className='header'>
             <nav className='header-content'>
@@ -20,19 +20,28 @@ export function Header() {
                     AuroraMap
                 </div>
                 <ul className={`header-nav-links ${isMenuBar ? 'header-is-menu-bar' : ' '}`}>
-                    <li><a href="#home">Início</a></li>
-                    <li><a href="#home-home-features">Recursos</a></li>
-                    <li><a href="#home-how-it-works">Como Funciona</a></li>
-                    <li><a href="#contact">Contato</a></li>
-                    <div className='header-buttons'>
-                        <Link href="/cadastro" className="register-button"> Cadastro</Link>
-                        <Link href="/login" className="login-button"> Login</Link>
-                    </div>
+                    {router !== '/mapa' ? (
+                        <>
+                            <li><a href="#home">Início</a></li>
+                            <li><a href="#home-home-features">Recursos</a></li>
+                            <li><a href="#home-how-it-works">Como Funciona</a></li>
+                            <li><a href="#contact">Contato</a></li>
+                            <div className='header-buttons'>
+                                <Link href="/cadastro" className="register-button"> Cadastro</Link>
+                                <Link href="/login" className="login-button"> Login</Link>
+                            </div>
+                        </>
+                    ) : (
+                        <li>
+                            <Link href="/" className="register-button">Home</Link>
+                        </li>
+                    )}
+
                 </ul>
 
                 <div className="header-button-menu-bar">
                     <button onClick={() => setIsMenuBar(!isMenuBar)}>
-                        
+
                         {isMenuBar ? <RiMenuFold4Line size={32} /> : <RiMenuFold3Line size={32} />}
                     </button>
                 </div>
